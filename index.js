c2bcb41958640c575d64d0044f253396dc75ab7f
lufangzhou <735417031@qq.com>
2018-09-05 11:11:51 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    //res.send('<h1>Hello world</h1>');
+    res.sendFile('/workspace/es6/client.html');
+});
+//io.emit('some event',{ for: 'everyone' });
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    //接受客户端发送事件
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message',msg);
+    });
+    //广播
+    //socket.broadcast.emit('hi');
+
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
---
+++ js/es6.js
@@ -0,0 +1,3 @@
+import {a,b} from './one'
+console.log(a,b);
+console.log('x');
