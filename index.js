9be9256f0e250656a6a72d1cd30bba864c897e1f
lufangzhou <735417031@qq.com>
2018-10-24 17:41:46 +0800

    es6

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
     //res.send('<h1>Hello world</h1>');
     res.sendFile('/workspace/es6/client.html');
 });
-//io.emit('some event',{ for: 'everyone' });
+io.emit('some event',{ for: 'everyone' });
 
 io.on('connection', function(socket){
     console.log('a user connected');
@@ -16,6 +16,8 @@ io.on('connection', function(socket){
     //接受客户端发送事件
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
+        console.log(20,'xxx',everyone);
+        sessionStorage.setItem('socket.io')
         io.emit('chat message',msg);
     });
     //广播
